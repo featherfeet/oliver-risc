71f0ee10af984f276c385e11a360c1723cf0a253
Oliver Lok Trevor <featherfeet5436@gmail.com>
2020-09-05 21:24:03 -0700

    Added MULT/DIV instructions to the hardware. Runs correctly in Icarus, not yet tried to synthesize.

--- hardware/rtl/top.v
+++ hardware/rtl/top.v
@@ -31,7 +31,7 @@ module top(
     output wire [35:0] GPIO_0
 );
 
-integer i; // Counter used for synthesized for loops.
+integer i; // Counter used for unrolled for-loops.
 
 // CPU registers.
 reg [`OPERATION_SIZE_BITS - 1:0] operation;
@@ -279,6 +279,22 @@ begin
 end
 endtask
 
+// Divider used for the DIV instruction (see alu/alu.v).
+reg division_state;
+reg [`OPERAND_SIZE_BITS - 1:0] divider_numerator;
+reg [`OPERAND_SIZE_BITS - 1:0] divider_denominator;
+wire [`OPERAND_SIZE_BITS - 1:0] divider_quotient;
+wire [`OPERAND_SIZE_BITS - 1:0] divider_remainder;
+lpm_divide #(.lpm_widthn(`OPERAND_SIZE_BITS), .lpm_widthd(`OPERAND_SIZE_BITS)) divider (
+    .numer(divider_numerator),
+    .denom(divider_denominator),
+    //.clock(),
+    //.aclr('b0),
+    //.clken('b1),
+    .quotient(divider_quotient),
+    .remain(divider_remainder)
+);
+
 always @(posedge CLOCK_50)
 begin
     // RESET LOGIC
@@ -326,6 +342,9 @@ begin
         code_section_start_address <= 'b0;
         code_section_start_found <= 'b0;
         program_end_address <= 'b0;
+        divider_numerator <= 'b0;
+        divider_denominator <= 'b0;
+        division_state <= `DIVISION_STATE_LATCH;
     end
     else
     begin
@@ -431,260 +450,7 @@ begin
             end
             `STATE_EXECUTE_INSTRUCTION:
             begin
-                case (operation)
-                    // No operation.
-                    `OPERATION_NOP:
-                    begin
-                        $display("NOP");
-                        next_instruction();
-                    end
-                    // Load value from RAM address to register.
-                    `OPERATION_LOAD:
-                    begin
-                        if (operand_byte_index == `OPERAND_SIZE_BYTES)
-                        begin
-                            $display("LOAD");
-                            $display("Loaded value %0d from address %0d in RAM to register %0d.", registers[operand2], operand1, operand2);
-                            operand_byte_index <= 'b0;
-                            next_instruction();
-                        end
-                        else if (ram_read_complete)
-                        begin
-                            registers[operand2] <= {ram_read_data[7:0], registers[operand2][31:8]};
-                            operand_byte_index <= operand_byte_index + 8'd1;
-                            ram_read_complete <= 'b0;
-                        end
-                        else
-                            read_from_ram(operand1 + operand_byte_index);
-                    end
-                    // Load value from RAM address (stored in a register) to a register.
-                    `OPERATION_RLOAD:
-                    begin
-                        if (ram_read_complete)
-                        begin
-                            registers[operand2] <= {ram_read_data[7:0], registers[operand2][31:8]};
-                            operand_byte_index <= operand_byte_index + 8'd1;
-                            ram_read_complete <= 'b0;
-                        end
-                        else
-                            read_from_ram(registers[operand1] + operand_byte_index);
-                        if (operand_byte_index == `OPERAND_SIZE_BYTES)
-                        begin
-                            $display("RLOAD");
-                            $display("Loaded value %0d from address %0d in RAM to register %0d.", registers[operand2], operand1, operand2);
-                            operand_byte_index <= 'b0;
-                            next_instruction();
-                        end
-                    end
-                    // Load value from constant operand into a register.
-                    `OPERATION_CLOAD:
-                    begin
-                        $display("CLOAD");
-                        registers[operand2] <= operand1;
-                        next_instruction();
-                    end
-                    // Store value into RAM from register.
-                    `OPERATION_STORE:
-                    begin
-                        if (operand_byte_index == `OPERAND_SIZE_BYTES)
-                        begin
-                            $display("STORE");
-                            sdram_controller_wr_n_i <= 'b1;
-                            operand_byte_index <= 'b0;
-                            next_instruction();
-                        end
-                        else if (ram_write_complete)
-                        begin
-                            operand_byte_index <= operand_byte_index + 8'd1;
-                            registers[operand1] <= {registers[operand1][7:0], registers[operand1][`OPERAND_SIZE_BITS - 1:8]};
-                            ram_write_complete <= 'b0;
-                        end
-                        else
-                        begin
-                            write_to_ram(operand2 + operand_byte_index, registers[operand1][7:0]);
-                        end
-                    end
-                    // Store value into RAM (at address specified by a register) from a register. registers[operand1] is the value being stored. registers[operand2] is the address in RAM to store at.
-                    `OPERATION_RSTORE:
-                    begin
-                        if (operand_byte_index == `OPERAND_SIZE_BYTES)
-                        begin
-                            $display("RSTORE");
-                            sdram_controller_wr_n_i <= 'b1;
-                            operand_byte_index <= 'b0;
-                            next_instruction();
-                        end
-                        else if (ram_write_complete)
-                        begin
-                            operand_byte_index <= operand_byte_index + 8'd1;
-                            registers[operand1] <= {registers[operand1][7:0], registers[operand1][`OPERAND_SIZE_BITS - 1:8]};
-                            ram_write_complete <= 'b0;
-                        end
-                        else
-                        begin
-                            write_to_ram(registers[operand2] + operand_byte_index, registers[operand1][7:0]);
-                        end
-                    end
-                    // Add two registers and store the result in register A.
-                    `OPERATION_ADD:
-                    begin
-                        $display("ADD");
-                        `REGISTER_A <= registers[operand1] + registers[operand2];
-                        next_instruction();
-                    end
-                    // Subtract two registers and store the result in register A.
-                    `OPERATION_SUB:
-                    begin
-                        $display("SUB");
-                        `REGISTER_A <= registers[operand1] - registers[operand2];
-                        next_instruction();
-                    end
-                    // Write to the I/O "memory" space (currently just the GPU's text buffer and the interrupt value). operand1 is the register number of the register containing the address to write to. operand2 is the register number of the register containing the value to write.
-                    `OPERATION_OUT:
-                    begin
-                        if (gpu_access_state == `GPU_ACCESS_STATE_SETUP && registers[operand1] < `GPU_TEXT_BUFFER_LENGTH)
-                        begin
-                            $display("OUT [address %0d, character '%c' (integer value %0d)]", registers[operand1], registers[operand2], registers[operand2]);
-                            gpu_write_enable <= 'b1;
-                            gpu_cell_to_access <= registers[operand1];
-                            gpu_character_to_write <= registers[operand2];
-                            gpu_access_state <= `GPU_ACCESS_STATE_FINISH;
-                        end
-                        else if (registers[operand1] == `INTERRUPT_VALUE_PORT)
-                        begin
-                            $display("Error: CPU cannot write to interrupt value port.");
-                            next_instruction(); // You cannot write to the interrupt value port.
-                        end
-                        else
-                        begin
-                            gpu_write_enable <= 'b0;
-                            gpu_access_state <= `GPU_ACCESS_STATE_SETUP;
-                            next_instruction();
-                        end
-                    end
-                    // Read from the I/O "memory" space. operand1 is the
-                    // register number of the register containing the port
-                    // number to read from. operand2 is the register number of the
-                    // register to read into.
-                    `OPERATION_IN:
-                    begin
-                        $display("IN");
-                        if (gpu_access_state == `GPU_ACCESS_STATE_SETUP && registers[operand1] < `GPU_TEXT_BUFFER_LENGTH)
-                        begin
-                            gpu_cell_to_access <= registers[operand1];
-                            gpu_access_state <= `GPU_ACCESS_STATE_FINISH;
-                        end
-                        else if (registers[operand1] == `INTERRUPT_VALUE_PORT)
-                        begin
-                            registers[operand2] <= interrupt_value_fifo_data_out;
-                            next_instruction();
-                        end
-                        else
-                        begin
-                            registers[operand2] <= {24'b0, gpu_character_read};
-                            gpu_access_state <= `GPU_ACCESS_STATE_SETUP;
-                            next_instruction();
-                        end
-                    end
-                    // Copy register 1 to register 2.
-                    `OPERATION_MOV:
-                    begin
-                        $display("MOV");
-                        registers[operand2] <= registers[operand1];
-                        next_instruction();
-                    end
-                    `OPERATION_CMP:
-                    begin
-                        $display("CMP register %0d (value %0d), register %0d (value %0d)", operand1, registers[operand1], operand2, registers[operand2]);
-                        if (registers[operand1] < registers[operand2])
-                            `REGISTER_A <= 'd0;
-                        else if (registers[operand1] == registers[operand2])
-                            `REGISTER_A <= 'd1;
-                        else
-                            `REGISTER_A <= 'd2;
-                        next_instruction();
-                    end
-                    `OPERATION_JMPL:
-                    begin
-                        $display("JMPL");
-                        if (`REGISTER_A == 0)
-                            `REGISTER_IP <= code_section_start_address + operand1;
-                        else
-                            `REGISTER_IP <= `REGISTER_IP + `INSTRUCTION_SIZE_BYTES;
-                        state <= `STATE_RUN_INTERRUPT;
-                    end
-                    `OPERATION_JMPE:
-                    begin
-                        $display("JMPE");
-                        if (`REGISTER_A == 1)
-                            `REGISTER_IP <= code_section_start_address + operand1;
-                        else
-                            `REGISTER_IP <= `REGISTER_IP + `INSTRUCTION_SIZE_BYTES;
-                        state <= `STATE_RUN_INTERRUPT;
-                    end
-                    `OPERATION_JMPG:
-                    begin
-                        $display("JMPG");
-                        if (`REGISTER_A == 2)
-                            `REGISTER_IP <= code_section_start_address + operand1;
-                        else
-                            `REGISTER_IP <= `REGISTER_IP + `INSTRUCTION_SIZE_BYTES;
-                        state <= `STATE_RUN_INTERRUPT;
-                    end
-                    `OPERATION_ISR:
-                    begin
-                        $display("ISR");
-                        interrupt_vector_table[registers[operand1]] <= operand2;
-                        next_instruction();
-                    end
-                    `OPERATION_INT:
-                    begin
-                        $display("INT");
-                        if (interrupt_fifo_access_state == `INTERRUPT_FIFO_ACCESS_STATE_SETUP)
-                        begin
-                            interrupt_fifo_data_in <= registers[operand1];
-                            interrupt_fifo_write <= 'b1;
-                            interrupt_fifo_access_state <= `INTERRUPT_FIFO_ACCESS_STATE_FINISH;
-                            interrupt_value_fifo_data_in <= 'b0;
-                            interrupt_value_fifo_write <= 'b1;
-                        end
-                        else
-                        begin
-                            state <= `STATE_RUN_INTERRUPT;
-                            interrupt_fifo_write <= 'b0;
-                            interrupt_value_fifo_write <= 'b0;
-                            interrupt_fifo_access_state <= `INTERRUPT_FIFO_ACCESS_STATE_SETUP;
-                        end
-                    end
-                    `OPERATION_RST:
-                    begin
-                        $display("RST");
-                        `REGISTER_IP <= code_section_start_address;
-                        `REGISTER_A <= 'b0;
-                        `REGISTER_B <= 'b0;
-                        `REGISTER_C <= 'b0;
-                        `REGISTER_D <= 'b0;
-                        `REGISTER_E <= 'b0;
-                        `REGISTER_F <= 'b0;
-                        `REGISTER_G <= 'b0;
-                        // TODO: Clear interrupt vector table and interrupt FIFO.
-                        state <= `STATE_RUN_INTERRUPT;
-                    end
-                    `OPERATION_ENDINT:
-                    begin
-                        $display("ENDINT");
-                        for (i = 0; i < `NUM_REGISTERS; i = i + 1)
-                            registers[i] <= shadow_registers[i];
-                        state <= `STATE_RUN_INTERRUPT;
-                        //`REGISTER_IR <= 'b0;
-                    end
-                    `OPERATION_HALT:
-                    begin
-                        $display("HALT");
-                        state <= `STATE_EXECUTE_INSTRUCTION;
-                        $finish;
-                    end
-                endcase
+                `include "alu.v"
             end
             // Every cycle, check for interrupts and run the topmost one on the FIFO.
             `STATE_RUN_INTERRUPT:
--- hardware/scripts/icarus.sh
+++ hardware/scripts/icarus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd rtl
-iverilog -o ../icarus_output -I gpu *.v gpu/*.v sdram/*.v -l libraries/altera_mf.v -l libraries/220model.v
+iverilog -o ../icarus_output -I gpu -I alu *.v gpu/*.v sdram/*.v -l libraries/altera_mf.v -l libraries/220model.v
 cd ..
