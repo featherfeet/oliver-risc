967ccc1fc6f23cb9a9a9b7b72bbebe5edf78230a
Oliver Lok Trevor <featherfeet5436@gmail.com>
2020-09-07 23:41:17 -0700

    Started working on timing the sim on different computers. Timed on olivers-desktop (the Supermicro server).

--- toolchain/compiler/build/output.asm
+++ toolchain/compiler/build/output.asm
@@ -2,77 +2,33 @@
     output_address = 0
     output_value = 0
     // Start variable declaration node.
-    PRIMES_TO_FIND = 0
+    i = 0
     // End variable declaration node.
     // Start variable declaration node.
-    primes_found = 0
-    // End variable declaration node.
-    // Start variable declaration node.
-    number_to_check = 0
-    // End variable declaration node.
-    // Start variable declaration node.
-    SCREEN_WIDTH = 0
-    // End variable declaration node.
-    // Start variable declaration node.
-    divisor = 0
-    // End variable declaration node.
-    // Start variable declaration node.
-    remainder = 0
-    // End variable declaration node.
-    // Start variable declaration node.
-    prime = 0
-    // End variable declaration node.
-    // Start variable declaration node.
-    num_digits = 0
-    // End variable declaration node.
-    // Start variable declaration node.
-    number_shifter = 0
-    // End variable declaration node.
-    // Start variable declaration node.
-    digit = 0
+    j = 0
     // End variable declaration node.
 .code:
     // Start root node.
     // Start variable declaration node.
-    CLOAD 24,A
-    STORE A,PRIMES_TO_FIND
+    CLOAD 1,A
+    STORE A,i
     // End variable declaration node.
     // Start variable declaration node.
     CLOAD 0,A
-    STORE A,primes_found
-    // End variable declaration node.
-    // Start variable declaration node.
-    CLOAD 2,A
-    STORE A,number_to_check
-    // End variable declaration node.
-    // Start variable declaration node.
-    CLOAD 80,A
-    STORE A,SCREEN_WIDTH
-    // End variable declaration node.
-    // Start variable declaration node.
-    CLOAD 2,A
-    STORE A,divisor
-    // End variable declaration node.
-    // Start variable declaration node.
-    CLOAD 0,A
-    STORE A,remainder
-    // End variable declaration node.
-    // Start variable declaration node.
-    CLOAD 0,A
-    STORE A,prime
+    STORE A,j
     // End variable declaration node.
     // Start while loop node.
     label0:
     // Start condition node.
     // Start expression node.
     CLOAD 0,A
-    LOAD primes_found,B
+    LOAD i,B
     ADD A,B
     // End expression node.
     MOV A,C
     // Start expression node.
     CLOAD 0,A
-    LOAD PRIMES_TO_FIND,B
+    CLOAD 5,B
     ADD A,B
     // End expression node.
     CMP C,A
@@ -83,271 +39,30 @@
     // Start variable assignment node.
     // Start expression node.
     CLOAD 0,A
-    CLOAD 2,B
-    ADD A,B
-    // End expression node.
-    STORE A,divisor
-    // End variable assignment node.
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,A
-    CLOAD 1,B
-    ADD A,B
-    // End expression node.
-    STORE A,prime
-    // End variable assignment node.
-    // Start while loop node.
-    label2:
-    // Start condition node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD divisor,B
-    ADD A,B
-    // End expression node.
-    MOV A,C
-    // Start expression node.
-    CLOAD 0,A
-    LOAD number_to_check,B
-    ADD A,B
-    // End expression node.
-    CMP C,A
-    // End condition node.
-    JMPE label3
-    JMPG label3
-    // Start begin . . . end block node.
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD number_to_check,B
-    ADD A,B
-    LOAD divisor,B
-    DIV A,B
-    MOV B,A
-    // End expression node.
-    STORE A,remainder
-    // End variable assignment node.
-    // Start conditional node.
-    // Start condition node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD remainder,B
-    ADD A,B
-    // End expression node.
-    MOV A,C
-    // Start expression node.
-    CLOAD 0,A
-    CLOAD 0,B
-    ADD A,B
-    // End expression node.
-    CMP C,A
-    // End condition node.
-    JMPL label4
-    JMPG label4
-    // Start begin . . . end block node.
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD number_to_check,B
-    ADD A,B
-    // End expression node.
-    STORE A,divisor
-    // End variable assignment node.
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,A
-    CLOAD 0,B
-    ADD A,B
-    // End expression node.
-    STORE A,prime
-    // End variable assignment node.
-    // End begin . . . end block node.
-    label4:
-    // End conditional node.
-    // Start conditional node.
-    // Start condition node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD remainder,B
-    ADD A,B
-    // End expression node.
-    MOV A,C
-    // Start expression node.
-    CLOAD 0,A
-    CLOAD 0,B
-    ADD A,B
-    // End expression node.
-    CMP C,A
-    // End condition node.
-    JMPE label5
-    JMPL label5
-    // Start begin . . . end block node.
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD divisor,B
+    LOAD i,B
     ADD A,B
     CLOAD 1,B
     ADD A,B
     // End expression node.
-    STORE A,divisor
-    // End variable assignment node.
-    // End begin . . . end block node.
-    label5:
-    // End conditional node.
-    // End begin . . . end block node.
-    CMP A,A
-    JMPE label2
-    label3:
-    // Start conditional node.
-    // Start condition node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD prime,B
-    ADD A,B
-    // End expression node.
-    MOV A,C
-    // Start expression node.
-    CLOAD 0,A
-    CLOAD 1,B
-    ADD A,B
-    // End expression node.
-    CMP C,A
-    // End condition node.
-    JMPL label6
-    JMPG label6
-    // Start begin . . . end block node.
-    // Start variable declaration node.
-    CLOAD 0,A
-    STORE A,num_digits
-    // End variable declaration node.
-    // Start variable declaration node.
-    CLOAD 0,A
-    STORE A,number_shifter
-    // End variable declaration node.
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD number_to_check,B
-    ADD A,B
-    // End expression node.
-    STORE A,number_shifter
-    // End variable assignment node.
-    // Start while loop node.
-    label7:
-    // Start condition node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD number_shifter,B
-    ADD A,B
-    // End expression node.
-    MOV A,C
-    // Start expression node.
-    CLOAD 0,A
-    CLOAD 0,B
-    ADD A,B
-    // End expression node.
-    CMP C,A
-    // End condition node.
-    JMPE label8
-    JMPL label8
-    // Start begin . . . end block node.
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD number_shifter,B
-    ADD A,B
-    CLOAD 10,B
-    DIV A,B
-    // End expression node.
-    STORE A,number_shifter
+    STORE A,i
     // End variable assignment node.
     // Start variable assignment node.
     // Start expression node.
     CLOAD 0,A
-    LOAD num_digits,B
-    ADD A,B
-    CLOAD 1,B
-    ADD A,B
-    // End expression node.
-    STORE A,num_digits
-    // End variable assignment node.
-    // End begin . . . end block node.
-    CMP A,A
-    JMPE label7
-    label8:
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD number_to_check,B
-    ADD A,B
-    // End expression node.
-    STORE A,number_shifter
-    // End variable assignment node.
-    // Start while loop node.
-    label9:
-    // Start condition node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD number_shifter,B
-    ADD A,B
-    // End expression node.
-    MOV A,C
-    // Start expression node.
-    CLOAD 0,A
-    CLOAD 0,B
-    ADD A,B
-    // End expression node.
-    CMP C,A
-    // End condition node.
-    JMPE label10
-    JMPL label10
-    // Start begin . . . end block node.
-    // Start variable declaration node.
-    CLOAD 0,A
-    STORE A,digit
-    // End variable declaration node.
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD number_shifter,B
-    ADD A,B
-    CLOAD 10,B
-    DIV A,B
-    MOV B,A
-    // End expression node.
-    STORE A,digit
-    // End variable assignment node.
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD primes_found,B
-    ADD A,B
-    LOAD SCREEN_WIDTH,B
-    MULT A,B
-    // End expression node.
-    STORE A,output_address
-    // End variable assignment node.
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD output_address,B
-    ADD A,B
-    LOAD num_digits,B
-    ADD A,B
-    CLOAD 1,B
-    SUB A,B
-    // End expression node.
-    STORE A,output_address
-    // End variable assignment node.
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD digit,B
+    LOAD i,B
     ADD A,B
     CLOAD 48,B
     ADD A,B
     // End expression node.
     STORE A,output_value
+    // End variable assignment node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,A
+    CLOAD 0,B
+    ADD A,B
+    // End expression node.
+    STORE A,output_address
     // End variable assignment node.
     // Start function call node.
     LOAD output_address,A
@@ -357,50 +72,36 @@
     // Start variable assignment node.
     // Start expression node.
     CLOAD 0,A
-    LOAD num_digits,B
+    LOAD i,B
     ADD A,B
-    CLOAD 1,B
-    SUB A,B
+    LOAD i,B
+    MULT A,B
     // End expression node.
-    STORE A,num_digits
+    STORE A,j
     // End variable assignment node.
     // Start variable assignment node.
     // Start expression node.
     CLOAD 0,A
-    LOAD number_shifter,B
+    LOAD j,B
     ADD A,B
-    CLOAD 10,B
-    DIV A,B
+    CLOAD 48,B
+    ADD A,B
     // End expression node.
-    STORE A,number_shifter
+    STORE A,output_value
     // End variable assignment node.
-    // End begin . . . end block node.
-    CMP A,A
-    JMPE label9
-    label10:
     // Start variable assignment node.
     // Start expression node.
     CLOAD 0,A
-    LOAD primes_found,B
-    ADD A,B
-    CLOAD 1,B
+    CLOAD 80,B
     ADD A,B
     // End expression node.
-    STORE A,primes_found
+    STORE A,output_address
     // End variable assignment node.
-    // End begin . . . end block node.
-    label6:
-    // End conditional node.
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,A
-    LOAD number_to_check,B
-    ADD A,B
-    CLOAD 1,B
-    ADD A,B
-    // End expression node.
-    STORE A,number_to_check
-    // End variable assignment node.
+    // Start function call node.
+    LOAD output_address,A
+    LOAD output_value,B
+    OUT A,B
+    // End function call node.
     // End begin . . . end block node.
     CMP A,A
     JMPE label0
