9302e54d9a3eda001fc502ce1473971de7f310dc
Oliver Lok Trevor <featherfeet5436@gmail.com>
2020-10-04 03:24:30 -0700

    Stack (with global variables being accessed from inside procedures) confirmed to work on the actual hardware!

--- toolchain/compiler/build/output.asm
+++ toolchain/compiler/build/output.asm
@@ -4,28 +4,453 @@
 .code:
     // Start root node.
     CLOAD 4000000,G // Start stack pointer at 4 MB, grows downwards (towards address 0).
-    CLOAD 12,A
+    CLOAD 44,A
     SUB G,A
     MOV A,G
     MOV G,F
     CLOAD 4,A
     ADD G,A
-    CLOAD 0,B
+    CLOAD 24,B
     RSTORE B,A
     CLOAD 8,A
     ADD G,A
-    CLOAD 10,B
+    CLOAD 0,B
     RSTORE B,A
-    // Start procedure node.
-    CMP A,A
-    JMPE end_procedure_prodc
-    start_procedure_prodc:
+    CLOAD 12,A
+    ADD G,A
+    CLOAD 2,B
+    RSTORE B,A
+    CLOAD 16,A
+    ADD G,A
+    CLOAD 80,B
+    RSTORE B,A
+    CLOAD 20,A
+    ADD G,A
+    CLOAD 2,B
+    RSTORE B,A
+    CLOAD 24,A
+    ADD G,A
+    CLOAD 0,B
+    RSTORE B,A
+    CLOAD 28,A
+    ADD G,A
+    CLOAD 0,B
+    RSTORE B,A
+    // Start while loop node.
+    label0:
+    // Start condition node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 8,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    MOV C,E
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 4,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CMP E,C
+    // End condition node.
+    JMPE label1
+    JMPG label1
     // Start begin . . . end block node.
     // Start variable assignment node.
     // Start expression node.
     CLOAD 0,C
+    CLOAD 2,D
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CLOAD 20,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 1,D
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CLOAD 28,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // Start while loop node.
+    label2:
+    // Start condition node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 20,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    MOV C,E
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 12,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CMP E,C
+    // End condition node.
+    JMPE label3
+    JMPG label3
+    // Start begin . . . end block node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 12,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    CLOAD 20,A
+    ADD G,A
+    RLOAD A,D
+
+    DIV C,D
+    MOV B,C
+    // End expression node.
+    CLOAD 24,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // Start conditional node.
+    // Start condition node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 24,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    MOV C,E
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 0,D
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CMP E,C
+    // End condition node.
+    JMPL label4
+    JMPG label4
+    // Start begin . . . end block node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 12,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CLOAD 20,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 0,D
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CLOAD 28,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // End begin . . . end block node.
+    label4:
+    // End conditional node.
+    // Start conditional node.
+    // Start condition node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 24,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    MOV C,E
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 0,D
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CMP E,C
+    // End condition node.
+    JMPE label5
+    JMPL label5
+    // Start begin . . . end block node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 20,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    CLOAD 1,D
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CLOAD 20,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // End begin . . . end block node.
+    label5:
+    // End conditional node.
+    // End begin . . . end block node.
+    CMP A,A
+    JMPE label2
+    label3:
+    // Start conditional node.
+    // Start condition node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 28,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    MOV C,E
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 1,D
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CMP E,C
+    // End condition node.
+    JMPL label6
+    JMPG label6
+    // Start begin . . . end block node.
+    CLOAD 32,A
+    ADD G,A
+    CLOAD 0,B
+    RSTORE B,A
+    CLOAD 36,A
+    ADD G,A
+    CLOAD 0,B
+    RSTORE B,A
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 12,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CLOAD 36,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // Start while loop node.
+    label7:
+    // Start condition node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 36,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    MOV C,E
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 0,D
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CMP E,C
+    // End condition node.
+    JMPE label8
+    JMPL label8
+    // Start begin . . . end block node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 36,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    CLOAD 10,D
+    DIV C,D
+    MOV A,C
+    // End expression node.
+    CLOAD 36,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 32,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    CLOAD 1,D
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CLOAD 32,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // End begin . . . end block node.
+    CMP A,A
+    JMPE label7
+    label8:
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 12,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CLOAD 36,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // Start while loop node.
+    label9:
+    // Start condition node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 36,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    MOV C,E
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 0,D
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CMP E,C
+    // End condition node.
+    JMPE label10
+    JMPL label10
+    // Start begin . . . end block node.
+    CLOAD 40,A
+    ADD G,A
+    CLOAD 0,B
+    RSTORE B,A
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 36,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    CLOAD 10,D
+    DIV C,D
+    MOV B,C
+    // End expression node.
+    CLOAD 40,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
     CLOAD 8,A
-    ADD F,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    CLOAD 16,A
+    ADD G,A
+    RLOAD A,D
+
+    MULT C,D
+    MOV A,C
+    // End expression node.
+    STORE C,output_address
+
+    // End variable assignment node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    LOAD output_address,D
+
+    ADD C,D
+    MOV A,C
+    CLOAD 32,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    CLOAD 1,D
+    SUB C,D
+    MOV A,C
+    // End expression node.
+    STORE C,output_address
+
+    // End variable assignment node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 40,A
+    ADD G,A
     RLOAD A,D
 
     ADD C,D
@@ -37,39 +462,94 @@
     STORE C,output_value
 
     // End variable assignment node.
-    // Start variable assignment node.
-    // Start expression node.
-    CLOAD 0,C
-    CLOAD 80,D
-    ADD C,D
-    MOV A,C
-    // End expression node.
-    STORE C,output_address
-
-    // End variable assignment node.
     // Start procedure call node.
     LOAD output_address,A
     LOAD output_value,B
     OUT A,B
     // End procedure call node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 32,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    CLOAD 1,D
+    SUB C,D
+    MOV A,C
+    // End expression node.
+    CLOAD 32,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 36,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    CLOAD 10,D
+    DIV C,D
+    MOV A,C
+    // End expression node.
+    CLOAD 36,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
     // End begin . . . end block node.
-    MOV G,B
-    CLOAD 4,A
-    ADD A,G
-    MOV A,G
-    RLOAD B,IP
-    end_procedure_prodc:
-    // End procedure node.
-    // Start procedure call node.
-    CLOAD 4,A
-    SUB G,A
-    MOV A,G
-    CLOAD 18,A
-    ADD IP,A
-    RSTORE A,G
     CMP A,A
-    JMPE start_procedure_prodc
-    // End procedure call node.
+    JMPE label9
+    label10:
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 8,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    CLOAD 1,D
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CLOAD 8,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // End begin . . . end block node.
+    label6:
+    // End conditional node.
+    // Start variable assignment node.
+    // Start expression node.
+    CLOAD 0,C
+    CLOAD 12,A
+    ADD G,A
+    RLOAD A,D
+
+    ADD C,D
+    MOV A,C
+    CLOAD 1,D
+    ADD C,D
+    MOV A,C
+    // End expression node.
+    CLOAD 12,A
+    ADD G,A
+    RSTORE C,A
+
+    // End variable assignment node.
+    // End begin . . . end block node.
+    CMP A,A
+    JMPE label0
+    label1:
     // Start procedure call node.
     HALT
     // End procedure call node.
